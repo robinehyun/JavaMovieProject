1ce3276b2c686f138c0228ddb5e68be17475a9a8
Robin Yun <robin.yun@zwift.com>
2024-01-24 14:18:40 -0800

    add review

--- src/main/java/dev/robin/movies/MovieService.java
+++ src/main/java/dev/robin/movies/MovieService.java
@@ -3,11 +3,16 @@ package dev.robin.movies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
---
+++ src/main/java/dev/robin/movies/ReviewService.java
@@ -0,0 +1,26 @@
+package dev.robin.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    private MongoTemplate mongoTemplate;
+    public Review createReviewBy(String reviewBody, String imdbId) {
+        Review review = new Review(reviewBody);
+        reviewRepository.insert(review);
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
+}
